d4c1c945a574b1a1361cdd7aed7eb1f9dbeaa32d
Gary <changary1994@gmail.com>
2022-05-26 21:24:10 -0400

    update movement script for enemy, to add AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,8 @@ public abstract class Enemy : MonoBehaviour
     protected float speed;
     protected Vector3 movementDestination;
     [SerializeField] protected Transform player;
+    protected Animator anim;
+
 
 
     bool isAttacked = false;
@@ -17,30 +19,42 @@ public abstract class Enemy : MonoBehaviour
     const int MAX_HEALTH = 10;
     protected void Start()
     {
-        
+        Init();
     }
 
     protected virtual void Init()
     {
-
+        anim = GetComponentInChildren<Animator>();
+        if (anim == null)
+        {
+            Debug.LogError(transform.name + "Animator is NULL.");
+        }
     }
     // Update is called once per frame
     protected virtual void Update()
     {
-        if(isAttacked && Time.time > canHit)
+        //if animation is idle and not in combat, then do not move this frame
+       if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle_anim") && !anim.GetBool("InCombat"))
         {
-            health -= 1;
-            canHit = Time.time + hitCooldown;
+            return;
         }
-        if (health == 0)
-        {
-            OnDeath();
-        }
-    }
 
+        Move();
+    }
+    // TODO: continue movement AI
     protected virtual void Move()
     {
-
+        //if not attacked, move to new position
+        if (!isAttacked)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movementDestination, speed * Time.deltaTime);
+        }
+        //if player moves out of range, stop combat
+        if (Vector3.Distance(player.position, transform.position) > 2f)
+        {
+            isAttacked = false;
+            anim.SetBool("InCombat", false);
+        }
     }
 
     protected virtual void Attack()
